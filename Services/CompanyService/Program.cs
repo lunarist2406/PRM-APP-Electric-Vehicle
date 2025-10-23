8fdabf0b3fe0a1929ecdac0f9097825bbd47764a
lunarist2406 <lyvanmy357@gmail.com>
2025-10-23 15:27:18 +0700

    update

--- Services/CompanyService/Program.cs
+++ Services/CompanyService/Program.cs
@@ -90,15 +90,13 @@ var app = builder.Build();
 // ==========================
 // 🔥 Middleware
 // ==========================
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CompanyService API V1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+  c.SwaggerEndpoint("/swagger/v1/swagger.json", "CompanyService API V1");
+  c.RoutePrefix = string.Empty;
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
