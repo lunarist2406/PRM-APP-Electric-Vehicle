accb89b1c073a7674d8ab5d0e622d7e856012cc0
lunarist2406 <lyvanmy357@gmail.com>
2025-11-02 14:57:06 +0700

    update

--- Services/ChargingPointService/Program.cs
+++ Services/ChargingPointService/Program.cs
@@ -136,15 +136,14 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // ✅ Middleware pipeline
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChargingPointService API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChargingPointService API v1");
+   c.RoutePrefix = string.Empty;
+});
+
 
 app.UseCustomCors();
 app.UseRateLimiter();
